e51bbb7b872fa41efc50c21b8da67a63cd2521c3
tim <1chloeprice@gmail.com>
2022-05-15 15:38:44 +0300

    moved plates

--- src/App.js
+++ src/App.js
@@ -2,11 +2,6 @@ import './App.css';
 import Header from './components/header'
 import Plate from './components/plate'
 import { Container } from 'react-bootstrap'
-import plateImage from './components/mainPlateLogo.png'
-import playerCount from './components/playerCountImage.png'
-import plateBg from './components/plateBg.png'
-
-
 
 function App() {
   return (
@@ -14,126 +9,7 @@ function App() {
       <Header />
 
       <Container>
-        <div className="plate-holder">
-
-          <div>
-
-            <div className="plate plate-long">
-              
-             <img className="long-plate-image" src={plateImage} height="50" display="inline"/>
-             <p className="long-plate-p">
-               TOTAL PROFIT
-               </p>
-               <p className="long-plate-p-value">
-               $142,944
-               </p>
-
-            </div>
-           
-            <div className="plate plate-long">
-              
-             <img className="long-plate-image" src={playerCount} height="50" display="inline"/>
-             <p className="long-plate-p">
-               PLAYER COUNT
-               </p>
-               <p className="long-plate-p-value">
-               10,283
-               </p>
-
-            </div>
-
-          </div>
-
-          <div className="plate">
-            <img src={plateImage} display="block" />
-            <p className="plate-title">10 BNB</p>
-            <p className="plate-description">PAYMENTS: 3
-              <br/>
-              PROFIT 75% PER 1
-            </p>
-
-            <button className="main-button">BUY</button>
-          </div>
-
-          <div className="plate">
-            <img src={plateImage} display="block" />
-            <p className="plate-title">10 BNB</p>
-            <p className="plate-description">PAYMENTS: 3
-              <br/>
-              PROFIT 75% PER 1
-            </p>
-
-            <button className="main-button">BUY</button>
-          </div>
-
-          <div className="plate">
-            <img src={plateImage} display="block" />
-            <p className="plate-title">10 BNB</p>
-            <p className="plate-description">PAYMENTS: 3
-              <br/>
-              PROFIT 75% PER 1
-            </p>
-
-            <button className="main-button">BUY</button>
-          </div>
-
-          <div className="plate">
-            <img src={plateImage} display="block" />
-            <p className="plate-title">10 BNB</p>
-            <p className="plate-description">PAYMENTS: 3
-              <br/>
-              PROFIT 75% PER 1
-            </p>
-
-            <button className="main-button">BUY</button>
-          </div>
-
-          <div className="plate">
-            <img src={plateImage} display="block" />
-            <p className="plate-title">10 BNB</p>
-            <p className="plate-description">PAYMENTS: 3
-              <br/>
-              PROFIT 75% PER 1
-            </p>
-
-            <button className="main-button">BUY</button>
-          </div>
-
-          <div className="plate">
-            <img src={plateImage} display="block" />
-            <p className="plate-title">10 BNB</p>
-            <p className="plate-description">PAYMENTS: 3
-              <br/>
-              PROFIT 75% PER 1
-            </p>
-
-            <button className="main-button">BUY</button>
-          </div>
-
-          <div className="plate">
-            <img src={plateImage} display="block" />
-            <p className="plate-title">10 BNB</p>
-            <p className="plate-description">PAYMENTS: 3
-              <br/>
-              PROFIT 75% PER 1
-            </p>
-
-            <button className="main-button">BUY</button>
-          </div>
-
-          <div className="plate">
-            <img src={plateImage} display="block" />
-            <p className="plate-title">10 BNB</p>
-            <p className="plate-description">PAYMENTS: 3
-              <br/>
-              PROFIT 75% PER 1
-            </p>
-
-            <button className="main-button">BUY</button>
-          </div>
-
-
-        </div>
+        <Plate/>
 
       </Container>
 
--- src/components/plate.js
+++ src/components/plate.js
@@ -1,11 +1,133 @@
 import React, { Component } from 'react'
+import plateImage from './mainPlateLogo.png'
+import playerCount from './playerCountImage.png'
+import plateBg from './plateBg.png'
+
+
 
 export default class Plate extends Component {
   render() {
     return (
-      <div className="plate-flex">
-          some text
+      <div className="plate-holder">
+
+          <div>
+
+            <div className="plate plate-long">
+              
+             <img className="long-plate-image" src={plateImage} height="50" display="inline"/>
+             <p className="long-plate-p">
+               TOTAL PROFIT
+               </p>
+               <p className="long-plate-p-value">
+               $142,944
+               </p>
+
+            </div>
+           
+            <div className="plate plate-long">
+              
+             <img className="long-plate-image" src={playerCount} height="50" display="inline"/>
+             <p className="long-plate-p">
+               PLAYER COUNT
+               </p>
+               <p className="long-plate-p-value">
+               10,283
+               </p>
+
+            </div>
+
           </div>
+
+          <div className="plate">
+            <img src={plateImage} display="block" />
+            <p className="plate-title">10 BNB</p>
+            <p className="plate-description">PAYMENTS: 3
+              <br/>
+              PROFIT 75% PER 1
+            </p>
+
+            <button className="main-button">BUY</button>
+          </div>
+
+          <div className="plate">
+            <img src={plateImage} display="block" />
+            <p className="plate-title">10 BNB</p>
+            <p className="plate-description">PAYMENTS: 3
+              <br/>
+              PROFIT 75% PER 1
+            </p>
+
+            <button className="main-button">BUY</button>
+          </div>
+
+          <div className="plate">
+            <img src={plateImage} display="block" />
+            <p className="plate-title">10 BNB</p>
+            <p className="plate-description">PAYMENTS: 3
+              <br/>
+              PROFIT 75% PER 1
+            </p>
+
+            <button className="main-button">BUY</button>
+          </div>
+
+          <div className="plate">
+            <img src={plateImage} display="block" />
+            <p className="plate-title">10 BNB</p>
+            <p className="plate-description">PAYMENTS: 3
+              <br/>
+              PROFIT 75% PER 1
+            </p>
+
+            <button className="main-button">BUY</button>
+          </div>
+
+          <div className="plate">
+            <img src={plateImage} display="block" />
+            <p className="plate-title">10 BNB</p>
+            <p className="plate-description">PAYMENTS: 3
+              <br/>
+              PROFIT 75% PER 1
+            </p>
+
+            <button className="main-button">BUY</button>
+          </div>
+
+          <div className="plate">
+            <img src={plateImage} display="block" />
+            <p className="plate-title">10 BNB</p>
+            <p className="plate-description">PAYMENTS: 3
+              <br/>
+              PROFIT 75% PER 1
+            </p>
+
+            <button className="main-button">BUY</button>
+          </div>
+
+          <div className="plate">
+            <img src={plateImage} display="block" />
+            <p className="plate-title">10 BNB</p>
+            <p className="plate-description">PAYMENTS: 3
+              <br/>
+              PROFIT 75% PER 1
+            </p>
+
+            <button className="main-button">BUY</button>
+          </div>
+
+          <div className="plate">
+            <img src={plateImage} display="block" />
+            <p className="plate-title">10 BNB</p>
+            <p className="plate-description">PAYMENTS: 3
+              <br/>
+              PROFIT 75% PER 1
+            </p>
+
+            <button className="main-button">BUY</button>
+          </div>
+
+
+        </div>
     )
   }
 }
