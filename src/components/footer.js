6555b88581c89ff2b3033eee24a308974df0881d
tim <1chloeprice@gmail.com>
2022-05-16 20:32:41 +0300

    123

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,8 +7,8 @@ export default class footer extends Component {
         return (
             <div>
              
-  <footer className="d-flex flex-wrap justify-content-between align-items-center border-top footer">
-    <p className="col-md-4 mb-0 text-muted">© 2021 Company, Inc</p>
+  <footer className="d-flex flex-wrap justify-content-between align-items-center border-top footer" style={{postiion:"fixed", bottom:"0px"}}>
+    <p className="col-md-4 mb-0 text-muted">© 2022 BNBgame</p>
 
     <a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
       <svg className="bi me-2" width="40" height="32"></svg>
@@ -16,9 +16,9 @@ export default class footer extends Component {
 
     <ul className="nav col-md-4 justify-content-end">
       <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Home</a></li>
-      <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Features</a></li>
-      <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Pricing</a></li>
-      <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">FAQs</a></li>
+      <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Referrals</a></li>
+      <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Support</a></li>
+      <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Telegram</a></li>
       <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">About</a></li>
     </ul>
   </footer>
